0716664bb2c54bdefc524f60876123a8f5ddf3cc
Mark Paxson <mpaxson@ist.ucf.edu>
2016-03-18 14:11:48 -0400

    initial commit

---
+++ app/app.module.js
@@ -0,0 +1,13 @@
+(function() {
+    'use strict';
+    
+    angular
+        .module('app', [
+            //angular modules.
+            'ngRoute',
+
+            //third party modules
+            'firebase'
+        ]);
+    
+})();
